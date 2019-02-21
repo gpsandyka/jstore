eb33be6232a1f7210be8f7861b2cf1a71d945574
Sandyka Gunnisyah Putra <sandyka.gunnisyah@ui.ac.id>
2019-02-21 18:02:54 +0700

    modul 1

---
+++ Database_Item.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class Database_Item here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Database_Item
+{
+    // instance variables - replace the example below with your own
+    private String listItem[];
+
+     /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void addItem()
+    {
+        // put your code here
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void removeItem()
+    {
+        // put your code here
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void getItemDatabase()
+    {
+        // put your code here
+    }
+}
---
+++ Item.java
@@ -0,0 +1,141 @@
+
+/**
+ * Write a description of class Item here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Item
+{
+    // instance variables - replace the example below with your own
+    protected int id;
+    protected String name;
+    protected int stock;
+    protected int price;
+    protected String category;
+
+    /**
+     * Constructor for objects of class Item
+     */
+    public Item()
+    {
+        // initialise instance variables
+        
+    }
+
+       
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getId()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String getName()
+    {
+        // put your code here
+        return "";
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getStock()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getPrice()
+    {
+        // put your code here
+        return 0;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String getCategory()
+    {
+        // put your code here
+        return "";
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void setId(int id)
+    {
+        // put your code here
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void setName(String name)
+    {
+        // put your code here
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void setStock(int stock)
+    {
+        // put your code here
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void setPrice(int price)
+    {
+        // put your code here
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void setCategory(String category)
+    {
+        // put your code here
+    }
+}
